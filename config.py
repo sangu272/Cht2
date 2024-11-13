7e2e7dcc216c6a909cc2beff1f0134f3c3ff4e66
sangu272 <sangeeta271713@gmail.com>
2024-11-13 13:13:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,6 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-SUPPORT_GRP = "https://t.me/+kIlKuGFNCiowZmY9"
+SUPPORT_GRP = "https://t.me/TG_BIO_STYLE"
 UPDATE_CHNL = "https://t.me/TG_BIO_STYLE"
 OWNER_USERNAME = "ll_ITZ_NAWAB_HERE_ll"
