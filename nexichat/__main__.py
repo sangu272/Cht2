1e327587484ea7034f4b54b181410be369fbb083
sangu272 <sangeeta271713@gmail.com>
2024-11-18 14:47:29 +0530

    Update __main__.py

--- nexichat/__main__.py
+++ nexichat/__main__.py
@@ -3,37 +3,57 @@ import asyncio
 import importlib
 from flask import Flask
 import threading
+import config
+from nexichat import ID_CHATBOT
 from pyrogram import idle
 from pyrogram.types import BotCommand
 from config import OWNER_ID
-from nexichat import LOGGER, nexichat, CLONE_OWNER
+from nexichat import LOGGER, nexichat, userbot, load_clone_owners
 from nexichat.modules import ALL_MODULES
-from nexichat.modules.Clone import restart_bots, load_clonebot_owner
+from nexichat.modules.Clone import restart_bots
+from nexichat.modules.Id_Clone import restart_idchatbots
+
 async def anony_boot():
-    global CLONE_OWNER
     try:
         await nexichat.start()
-        await restart_bots()
-        CLONE_OWNER = await load_clonebot_owner()
-        print(f"{CLONE_OWNER}")
+        try:
+            await nexichat.send_message(int(OWNER_ID), f"**{nexichat.mention} Is started✅**")
+        except Exception as ex:
+            LOGGER.info(f"@{nexichat.username} Started, please start the bot from owner id.")
+    
+        asyncio.create_task(restart_bots())
+        asyncio.create_task(restart_idchatbots())
+        await load_clone_owners()
+        if config.STRING1:
+            try:
+                await userbot.start()
+                try:
+                    await nexichat.send_message(int(OWNER_ID), f"**Id-Chatbot Also Started✅**")
+                except Exception as ex:
+                    LOGGER.info(f"@{nexichat.username} Started, please start the bot from owner id.")
+    
+            except Exception as ex:
+                print(f"Error in starting id-chatbot :- {ex}")
+                pass
     except Exception as ex:
         LOGGER.error(ex)
-        
 
     for all_module in ALL_MODULES:
         importlib.import_module("nexichat.modules." + all_module)
         LOGGER.info(f"Successfully imported : {all_module}")
 
-    # Set bot commands
+    
     try:
-        
         await nexichat.set_bot_commands(
             commands=[
                 BotCommand("start", "Start the bot"),
                 BotCommand("help", "Get the help menu"),
                 BotCommand("clone", "Make your own chatbot"),
+                BotCommand("idclone", "Make your id-chatbot"),
+                BotCommand("cloned", "Get List of all cloned bot"),
                 BotCommand("ping", "Check if the bot is alive or dead"),
                 BotCommand("lang", "Select bot reply language"),
+                BotCommand("chatlang", "Get current using lang for chat"),
                 BotCommand("resetlang", "Reset to default bot reply lang"),
                 BotCommand("id", "Get users user_id"),
                 BotCommand("stats", "Check bot stats"),
@@ -41,25 +61,20 @@ async def anony_boot():
                 BotCommand("chatbot", "Enable or disable chatbot"),
                 BotCommand("status", "Check chatbot enable or disable in chat"),
                 BotCommand("shayri", "Get random shayri for love"),
+                BotCommand("ask", "Ask anything from chatgpt"),
                 BotCommand("repo", "Get chatbot source code"),
             ]
         )
         LOGGER.info("Bot commands set successfully.")
     except Exception as ex:
         LOGGER.error(f"Failed to set bot commands: {ex}")
-
+    
     LOGGER.info(f"@{nexichat.username} Started.")
-    try:
-        await nexichat.send_message(int(OWNER_ID), f"{nexichat.mention} has started")
-    except Exception as ex:
-        LOGGER.info(f"@{nexichat.username} Started, please start the bot from owner id.")
     
     await idle()
 
 
-# Flask Server Code for Health Check
 app = Flask(__name__)
-
 @app.route('/')
 def home():
     return "Bot is running"
@@ -68,10 +83,7 @@ def run_flask():
     app.run(host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
-    # Start Flask server in a new thread
     flask_thread = threading.Thread(target=run_flask)
     flask_thread.start()
-
-    # Start the bot asynchronously
     asyncio.get_event_loop().run_until_complete(anony_boot())
     LOGGER.info("Stopping nexichat Bot...")
