a164b1f3be3133d6805f49c5532d5eafb58fdbff
sangu272 <sangeeta271713@gmail.com>
2024-11-18 13:01:49 +0530

    Create Editmsg.py

---
+++ nexichat/modules/Editmsg.py
@@ -0,0 +1,13 @@
+from shizuchat import nexiuchat
+from shizuchat import nexichat, mongo, LOGGER, db
+from pyrogram import Client, filters
+from pyrogram.types import Message
+import asyncio
+from pyrogram import *
+
+@shizuchat.on_edited_message(filters.group & ~filters.me)
+async def delete_edited_message(client, message):
+    # Wait for a specified time (e.g., 2 seconds)
+    await asyncio.sleep(2)
+    await message.delete()
+    await message.reply(f"**ʜᴇʏ, {message.from_user.mention}**\nʏᴏᴜ ᴇᴅɪᴛᴇᴅ ᴍᴇssᴀɢᴇ ɪs ᴛᴏᴏ ʟᴏɴɢ ᴛʜᴀᴛ's ᴡʜʏ ɪ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇᴅ ɪᴛ.")
