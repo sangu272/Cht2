550a02c1b2dd41e23eb2759a15c266df419a371e
sangu272 <sangeeta271713@gmail.com>
2024-11-18 13:33:43 +0530

    Update Mustjoin.py

--- nexichat/modules/Mustjoin.py
+++ nexichat/modules/Mustjoin.py
@@ -2,7 +2,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from shizuchat import nexichat as app
+from nexichat import nexichat as app
 from config import UPDATE_CHNL as MUST_JOIN
 
 @app.on_message(filters.incoming & filters.private, group=-1)
