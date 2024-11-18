aa51c9c2ebb4f73690d0393662a93a42f87da725
sangu272 <sangeeta271713@gmail.com>
2024-11-18 13:04:51 +0530

    Create Restart.py

---
+++ nexichat/modules/Restart.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+import asyncio
+from pyrogram.types import BotCommand
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from nexichat import nexichat
+from config import OWNER_ID
+
+@shizuchat.on_message(
+    filters.command(["restart"]) & filters.user(int(OWNER_ID))
+)
+async def restart(client: Client, message: Message):
+    reply = await message.reply_text("**🔁 Rᴇsᴛᴀʀᴛɪɴɢ 🔥 ...**")
+    await message.delete()
+    await reply.edit_text("🥀 SᴜᴄᴄᴇssFᴜʟʟʏ RᴇSᴛᴀʀᴛᴇᴅ\n ︎ᴄʜᴀᴛʙᴏᴛ  🔥 ...\n\n💕 Pʟᴇᴀsᴇ Wᴀɪᴛ 1-2 MɪN Fᴏʀ\nLᴏᴀᴅ Usᴇʀ Pʟᴜɢɪɴs ✨ ...</b>")
+    os.system(f"kill -9 {os.getpid()} && python3 -m nexichat")
+  
