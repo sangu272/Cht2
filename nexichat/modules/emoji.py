d1f86c482d245686bc5204d549b3b67bff7a58be
sangu272 <sangeeta271713@gmail.com>
2024-11-18 17:12:40 +0530

    Update emoji.py

--- nexichat/modules/emoji.py
+++ nexichat/modules/emoji.py
@@ -119,4 +119,55 @@ rtemoji = [
     "⁭\n{cj}{cj}{cj}{cj}{cj}{cj}\n{cj}{cj}{cj}{cj}{cj}{cj}\n{cj}{cj}\n {cj}{cj}{cj}{cj}{cj}\n   {cj}{cj}{cj}{cj}{cj}\n                    {cj}{cj}\n                    {cj}{cj}\n{cj}{cj}          {cj}{cj}\n  {cj}{cj}{cj}{cj}{cj}\n     {cj}{cj}{cj}{cj}\n",
     "⁭\n        {cj}{cj}{cj}{cj}\n    {cj}{cj}{cj}{cj}{cj}\n{cj}{cj}\n\n{cj}{cj}\n{cj}{cj}{cj}{cj}{cj}{cj}\n{cj}{cj}{cj}{cj}{cj}{cj}{cj}\n{cj}{cj}               {cj}{cj}\n{cj}{cj}               {cj}{cj}\n    {cj}{cj}{cj}{cj}{cj}{cj}\n        {cj}{cj}{cj}{cj}\n",
     "⁭\n{cj}{cj}{cj}{cj}{cj}{cj}{cj}\n{cj}{cj}{cj}{cj}{cj}{cj}{cj}\n                      {cj}{cj}\n                     {cj}{cj}\n                   {cj}{cj}\n                 {cj}{cj}\n               {cj}{cj}\n             {cj}{cj}\n           {cj}{cj}\n         {cj}{cj}\n",
-    "⁭\n        {cj}{cj}{cj}{cj}\n   {cj}{cj}{cj}{cj}{cj}{cj}\n{cj}{cj}               {cj}{cj}\n{cj}{cj}               {cj}{cj}\n   {cj}{cj}{cj}{cj}{cj}{cj}\n   {cj}{cj}{cj}{cj}{cj}{cj}\n
+    "⁭\n        {cj}{cj}{cj}{cj}\n   {cj}{cj}{cj}{cj}{cj}{cj}\n{cj}{cj}               {cj}{cj}\n{cj}{cj}               {cj}{cj}\n   {cj}{cj}{cj}{cj}{cj}{cj}\n   {cj}{cj}{cj}{cj}{cj}{cj}\n{cj}{cj}               {cj}{cj}\n{cj}{cj}               {cj}{cj}\n   {cj}{cj}{cj}{cj}{cj}{cj}\n        {cj}{cj}{cj}{cj}\n",
+    "⁭\n        {cj}{cj}{cj}{cj}\n   {cj}{cj}{cj}{cj}{cj}{cj}\n{cj}{cj}               {cj}{cj}\n{cj}{cj}               {cj}{cj}\n {cj}{cj}{cj}{cj}{cj}{cj}{cj}\n      {cj}{cj}{cj}{cj}{cj}{cj}\n                         {cj}{cj}\n                        {cj}{cj}\n  {cj}{cj}{cj}{cj}{cj}{cj}\n       {cj}{cj}{cj}{cj}\n",
+]
+
+
+@shizuchat.on_cmd(["emoji"])
+async def emoji(client: Client, message: Message):
+    op = await edit_or_reply(message, "`Emojifying the text..`")
+    args = get_text(message)
+    if not args:
+        if not message.reply_to_message:
+           return await ok.edit("__What am I Supposed to do with this idiot, Give me a text.__")
+        if not message.reply_to_message.text:
+           return await ok.edit("__What am I Supposed to do with this idiot, Give me a text.__")
+    args = args or message.reply_to_message.text
+    
+    result = ""
+    for a in args:
+        a = a.lower()
+        if a in rtext:
+            char = remoji[rtext.index(a)]
+            result += char
+        else:
+            result += a
+    await op.edit(result)
+    
+
+@shizuchat.on_cmd(["love"])
+async def cmoji(client: Client, message: Message):
+    op = await edit_or_reply(message, "`Emojifying the text..`")
+    args = get_text(message)
+    if not args:
+        if not message.reply_to_message:
+           return await ok.edit("__What am I Supposed to do with this idiot, Give me a text.__")
+        if not message.reply_to_message.text:
+           return await ok.edit("__What am I Supposed to do with this idiot, Give me a text.__")
+    args = args or message.reply_to_message.text
+    try:
+        emoji, arg = args.split(" ", 1)
+    except Exception:
+        arg = args
+        emoji = "❤️"
+    result = ""
+    for a in arg:
+        a = a.lower()
+        if a in rtext:
+            char = rtemoji[rtext.index(a)].format(cj=emoji)
+            result += char
+        else:
+            result += a
+    await op.edit(result)
+    
